5f0acbd1a5586fd10bc2d484418a68fc345e61e4
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-07-19 18:13:20 -0700

    added image and text to homepage

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Parallax } from "react-parallax";
+import Sky from '../../assets/sky.jpg';
+import './home.css';
+
+export default function Home() {
+    return (
+    <Parallax
+    
+    bgImage={Sky}
+    bgImageAlt="a sky with snow falling"
+    strength={800}
+    className="home-image"
+  >
+        <div className='container' id="home">
+          <div className='title'>
+            <p className='intro'> LOS ANGELES</p>
+            <p className='mount'>MOUNTAINS</p>
+          </div>
+        </div>
+        </Parallax>
+    )
+}
