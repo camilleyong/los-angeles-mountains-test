673b45d8dfc497cef92b8ed66b5d0919cd4aca48
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-07-24 23:33:11 -0700

    fixed nav

--- src/components/Mountains.js
+++ src/components/Mountains.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nav from './Nav';
+// import Nav from './Nav';
 import Home from './pages/Home';
 // import Footer from './Footer';
 import './css/mountains.css';
@@ -8,7 +8,7 @@ import './css/mountains.css';
 export default function PortfolioContainer() {
     return (
         <div className='entirePage'>
-            {<Nav />}
+            {/* {<Nav />} */}
             {<Home />}
             {/* {<Footer />} */}
         </div>
