2669fd1670e8689273a5819c7190b109dc10e0bf
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-07-24 23:00:39 -0700

    fixed spacing and sizing

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/nav.css";
+
+export default function Nav() {
+  return (
+    <div className="navigation ">
+      <ul>
+        <li>
+          <a href="" className="nav-link">Team
+          </a>
+        </li>
+        <li>
+          <a href="" className="nav-link">History
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
