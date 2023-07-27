a03615232b8c83f616c86246ed35f91c5dd00cf9
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-07-26 22:19:52 -0700

    added nav files

---
+++ src/components/pages/History/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './history.css';
+import One from '../../assets/mountains.jpg';
+
+
+
+export default function History() {
+    return (
+        <div style={{ backgroundImage: `url(${One})` }}>
+        Hello World
+      </div>
+
+    );
+
+};
